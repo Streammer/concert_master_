0ad850e6fd73796f6045b14a4899d25748836747
Dmitrij <lukdmitrij@yandex.ru>
2024-10-11 23:43:10 +0300

    test api

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
-<script setup>
-import { useMovieStore } from "./stores/movies_store";
-const movieStore = useMovieStore();
-</script>
-
 <template>
   <div>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import axios from 'axios';
+import { useMovieStore } from "./stores/movies_store";
+import { ref, onMounted } from 'vue';
+
+const baseUrl = import.meta.env.VITE_PUBLIC_BASE_API_URL;
+const movieStore = useMovieStore();
+
+const getMovies = async () => {
+  await axios.get(baseUrl)
+    .then(response => {
+      console.log(response.data);
+      movieStore.movies = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching movies:', error);
+    });
+};
+
+onMounted(() => {
+  getMovies();
+});
+</script>
+
+<style scoped></style>
--- src/stores/movies_store.js
+++ src/stores/movies_store.js
@@ -2,8 +2,10 @@ import { defineStore } from "pinia";
 import { ref, computed, watch } from "vue";
 
 export const useMovieStore = defineStore("movieStore", () => {
-  const movies = ref(['1', '2', '3']);
-
+  const movies = ref([]);
+  const setMovies = (data) => {
+    movies.value = data;
+  }  
 
   return {
     movies,
