f694da17062bfc9b7854412dd489c82b06d95413
Dmitrij <lukdmitrij@yandex.ru>
2024-10-12 10:08:30 +0300

    install sass

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+    <header class="header">
+        <h1 class="header__title">Header</h1>
+    </header>
 </template>
+
+<script setup>
+</script>
+<style lang="scss" scoped>
+ .header {
+     width: 100%;
+     &__title {
+        color: red;
+     }
+ }
+</style>
